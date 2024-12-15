4a683974444c21a0a4ae280d56309e8b93cb2271
Roberto Gianotto <gianottoroberto@gmail.com>
2024-12-15 08:40:04 +0100

    aggiunto algoritmo di validazione stringhe in uppercase e lowercase

---
+++ Ex_Files_Java_Algorithms/Exercise Files/01/01_01/start/Algorithms/src/it/java/algoritmi/validazionestringhe/Algorithms.java
@@ -0,0 +1,28 @@
+package it.java.algoritmi.validazionestringhe;
+
+public class Algorithms {
+
+    /**
+     * Questo algoritmo convalida delle stringhe
+     * @param s
+     * @return
+     */
+
+    public static boolean isUppercase(String s) {
+        // questa funzione si applica ai singoli caraateri di una stringa e non alla stringa in sè
+        return s.chars().allMatch(Character::isUpperCase);
+    }
+
+    public static boolean isLowercase(String s) {
+        // nessun match con Uppercase
+        return s.chars().allMatch(Character::isLowerCase);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isUppercase("hello"));
+        System.out.println(isUppercase("HELLO"));
+        System.out.println("===================");
+        System.out.println(isLowercase("hello"));
+        System.out.println(isLowercase("HELLO"));
+    }
+}
